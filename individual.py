3242f29d788e32e6ab5e00864a9c336ad1caeff4
landrumb <36489943+landrumb@users.noreply.github.com>
2020-04-07 11:43:50 -0400

    Wrote class docstring for Individual
    Signed-off-by: landrumb <36489943+landrumb@users.noreply.github.com>

--- individual.py
+++ individual.py
@@ -2,20 +2,23 @@ class Individual:
     """ Class that represents an individual
     
     Attributes: 
-        genes: default is None
-        fit_func (func): 
-                
+        genes(array): array of 16 floats representing the individual's characteristics
+        fit_func(func): function used to assess fitness of the individual
+        fitness(float): a float representing the individual's fitness, stored so it doesn't have to be evaluated every
+            time the value is used, default None
+
+    (Ben)
     """
 
     def __init__(self, genes, fit_func):
         """
         Args:
-            genes(array): an array of 16 attributes representing the individual's characteristics
+            genes(array): an array of 16 floats representing the individual's characteristics
             fit_func(func): function for assessing fitness
         """
         self.genes = genes
         self.fit_func = fit_func
-        self.fitness = -1
+        self.fitness = None
 
     def assess_fit(self):
         """ Assesses fitness based on self.fit_func and stores it it in self.fit"""
