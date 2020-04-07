ea86e1d25a20067fb1f7cc4062347e5017c51657
Yazeed <Yazeed@10.104.165.21>
2020-04-06 20:19:04 -0400

    generation and unittest classes

--- generation.py
+++ generation.py
@@ -1,1 +1,29 @@
-
+class Generation:
+    """ This class represents a generaion of individuals
+    
+    Attributes:
+        individuals (list): list of individuals
+        fitness_func (func): function used to assess fitness of individuals
+    
+    """
+    
+    def __init__(self, indiv, fit_func):
+        self.individuals = indiv
+        self.fitness_func = fit_func
+         
+    def reproduction(self):
+        """ Produces the next generation
+        
+        Returns:
+            Generation: new generation object consisting of individuals 
+            decended from self.individuals
+        """
+    
+    def fitness(self):
+        """ Assess the fitness of individuals in the generation by calling 
+        fitness method of members self.individuals """
+        
+    
+    
+        
+    
--- unittest.py
+++ unittest.py
@@ -1,1 +1,8 @@
+import unittest
+import generation as gen 
+import individual as ind
+import population as pop
 
+class TestGeneticAlgo(unittest.TestCase):
+    
+    
