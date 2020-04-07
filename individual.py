781ebb8ac20f13ea54ea8b1306781e728b247f04
The Run <TheRun98@users.noreply.github.com>
2020-04-06 20:46:06 -0400

    Update individual.py

--- individual.py
+++ individual.py
@@ -1,1 +1,28 @@
-
+class Individual:
+    """
+    Attributes: genes which is default to None
+                fit_func
+                fitness = None
+                
+    """
+    def __init__(self, genes, fit_func):
+        self.genes = genes
+        self.fit_func = fit_func
+        
+    def assess_fit(self):
+        
+    """
+    Assesses fitness based on self.fit_func
+    """
+        ## self.fitness = fitness
+    
+    def reproduce(self, other):
+    """
+    Creates children/child with parents 'self' and 'other'.
+    Args: 
+        other (individual): Mate with which self has a child    
+    Returns:
+        individual: child individual with parents self and other
+    
+    
+    """
