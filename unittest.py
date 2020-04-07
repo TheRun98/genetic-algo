eb50e7f15bb288c09ea0e8e29ebb1916a7cc8e2e
Yazeed <Yazeed@10.104.165.21>
2020-04-06 21:31:07 -0400

    added unit tests

--- unittest.py
+++ unittest.py
@@ -4,3 +4,22 @@ import individual as indv
 import population as pop 
 
 class TestGeneticAlgo(unittest.TestCase):
+    
+    def setUp(self):
+        genes = []
+        func = None
+        self.individual = indv.Individual(genes, func)
+        self.generation = gen.Generation([self.individual])
+        self.population = pop.Population(func)
+        
+    def test_generation(self):
+        self.assertTrue(self.generation.__str__() == "")
+        
+    def test_individual(self):
+        self.assertTrue(self.individual.__str__() == "")
+        
+    def test_population(self):
+        self.assertTrue(self.population.__str__() == "")
+        
+if __name__ == "__main__":
+    unittest.main()
