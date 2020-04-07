8b3a1179a642ca3f0e75140cf63f0ba51ed32b9c
landrumb <36489943+landrumb@users.noreply.github.com>
2020-04-07 12:56:25 -0400

    added test_individual_assess_fit() and test_generation_fitness
    Signed-off-by: landrumb <36489943+landrumb@users.noreply.github.com>

--- test_project.py
+++ test_project.py
@@ -1,14 +1,15 @@
 import unittest
 import numpy as np
+from numpy.polynomial.polynomial import polyval
 from generation import Generation
 from individual import Individual
 from population import Population
 
 np.random.seed(0)
-TARGET = np.random.rand((16,))
-RANDOM_GENES_A = np.random.rand((16,))
-RANDOM_GENES_B = np.random.rand((16,))
-RANDOM_GENES_C = np.random.rand((16,))
+TARGET = np.random.rand(16)
+RANDOM_GENES_A = np.random.rand(16)
+RANDOM_GENES_B = np.random.rand(16)
+RANDOM_GENES_C = np.random.rand(16)
 
 
 def dummy_func(input_array):
@@ -25,8 +26,8 @@ def dummy_func(input_array):
     """
     n_samples = 10_000
     test_range = np.linspace(0, 100, n_samples)
-    target = np.polynomial.polyval(test_range, TARGET, tensor=False)
-    output = np.polynomial.polyval(test_range, input_array, tensor=False)
+    target = polyval(test_range, TARGET, tensor=False)
+    output = polyval(test_range, input_array, tensor=False)
     loss = np.sum(abs(target - output)) / n_samples
     return loss
 
@@ -42,13 +43,35 @@ class TestGeneticAlgo(unittest.TestCase):
         self.population = Population(0.25, func)
 
     def test_generation(self):
-        self.assertTrue(self.generation.__str__() == "")
+        self.assertTrue(self.generation.__str__() == "None: [0.0202184  0.83261985 0.77815675 0.87001215 0.97861834 "
+                                                     "0.79915856\n 0.46147936 0.78052918 0.11827443 0.63992102 "
+                                                     "0.14335329 0.94466892\n 0.52184832 0.41466194 0.26455561 "
+                                                     "0.77423369]\nNone: [0.45615033 0.56843395 0.0187898  0.6176355  "
+                                                     "0.61209572 0.616934\n 0.94374808 0.6818203  0.3595079  0.43703195"
+                                                     " 0.6976312  0.06022547\n 0.66676672 0.67063787 0.21038256 "
+                                                     "0.1289263 ]\nNone: [0.31542835 0.36371077 0.57019677 0.43860151 "
+                                                     "0.98837384 0.10204481\n 0.20887676 0.16130952 0.65310833 0.2532916"
+                                                     "  0.46631077 0.24442559\n 0.15896958 0.11037514 0.65632959 "
+                                                     "0.13818295]\n")
 
     def test_individual(self):
-        self.assertTrue(self.individual_a.__str__() == "")
+        self.assertTrue(self.individual_a.__str__() == "None: [0.0202184  0.83261985 0.77815675 0.87001215 0.97861834 "
+                                                       "0.79915856\n 0.46147936 0.78052918 0.11827443 0.63992102 "
+                                                       "0.14335329 0.94466892\n 0.52184832 0.41466194 0.26455561 "
+                                                       "0.77423369]")
 
     def test_population(self):
         self.assertTrue(self.population.__str__() == "")
+
+    def test_individual_assess_fit(self):
+        """(Ben)"""
+        self.individual_a.assess_fit()
+        self.assertAlmostEqual(self.individual_a.fitness, 4.309953574343199e+28)
+
+    def test_generation_fitness(self):
+        """(Ben)"""
+        self.generation.fitness()
+        self.assertAlmostEqual(self.generation.individuals[0].fitness, 4.309953574343199e+28)
 
 
 if __name__ == "__main__":
