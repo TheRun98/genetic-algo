541714aa064398c02fb739249280ef4ea64854de
landrumb <36489943+landrumb@users.noreply.github.com>
2020-04-07 10:48:21 -0400

    updated docstrings and added __str__() method to individual.py
    Signed-off-by: landrumb <36489943+landrumb@users.noreply.github.com>

--- individual.py
+++ individual.py
@@ -6,14 +6,22 @@ class Individual:
         fit_func (func): 
                 
     """
+
     def __init__(self, genes, fit_func):
+        """
+
+        Args:
+            genes(array): an array of 16 attributes representing the individual's characteristics
+            fit_func(func): function for assessing fitness
+        """
         self.genes = genes
         self.fit_func = fit_func
-        
-    def assess_fit(self):   
-         """ Assess fitness based on self.fit_func """
-        ## self.fitness = fitness
-    
+        self.fitness = -1
+
+    def assess_fit(self):
+        """ Assesses fitness based on self.fit_func and stores it it in self.fit"""
+        # self.fitness = self.fit_func(self.genes)
+
     def reproduce(self, other):
         """ Creates child with parents 'self' and 'other'.
         Args:
@@ -21,3 +29,6 @@ class Individual:
         Returns:
             individual: child individual with parents self and other
         """
+
+    def __str__(self):
+        return str(self.fitness) + ": " + str(self.genes)
